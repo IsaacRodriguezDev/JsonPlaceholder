d8311b9c635825e8edfa5886895748abcb22f5f2
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-06-04 14:51:29 -0500

    added dictionary page and some navbars

---
+++ scripts/dictionary.js
@@ -0,0 +1,30 @@
+"use strict";
+window.onload = () => {
+  let button = document.querySelector("#theButton");
+  button.addEventListener("click", searchWord);
+};
+
+async function searchWord() {
+  let userInput = document.querySelector("#searchWord");
+  let results = document.querySelector("#results");
+  let url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  let userUrl = url + userInput.value;
+  if(userInput.value ===''){
+   return results.innerHTML = 'Nice try! Enter a Word'
+  }
+  try {
+    let response = await fetch(userUrl);
+    if(!response.ok){
+      throw new Error(`UH OH! ERROR: ${response.status}`) 
+    }
+    let data = await response.json();
+data.forEach((datas,idx)=>{
+    results.innerHTML = JSON.stringify(datas.meanings[idx].definitions);
+})
+    
+    
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
